2fb41dd4b5288a3fec1b8507f2a3e042441a9341
Haresh Venkatesan <130588197+Haresh-V-2003@users.noreply.github.com>
2025-04-23 15:03:00 +0530

    Update load-test.js

--- load-test.js
+++ load-test.js
@@ -7,7 +7,10 @@ export let options = {
     { duration: '30s', target: 50 },  // Stay at 50 users for 30 seconds
     { duration: '10s', target: 0 },  // Ramp-down to 0 users over 10 seconds
   ],
-  
+  thresholds: {
+    http_req_failed: ['rate<0.00'],
+    http_req_duration: ['p(100)<2000'],
+  },
   ext: {
     loadimpact: {
       projectID: __ENV.K6_PROJECT_ID, // Replace with your Grafana Cloud project ID
